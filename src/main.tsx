61255316408fd9aa40ffff163ba273d856f49424
mayankgupta804 <mayank.gupta804@gmail.com>
2025-04-22 19:28:17 +0530

    Remove unused css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import theme from './theme'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
