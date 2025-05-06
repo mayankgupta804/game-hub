b19898d528c98991a16a13db7ccf62f38f7834bb
mayankgupta804 <mayank.gupta804@gmail.com>
2025-05-06 21:58:03 +0530

    Show video game genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,30 +1,30 @@
 import { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
+import { Game, Genre } from "./useGames";
 
-export interface Platform {
-  id: string;
-  game_id: string;
-  name: string;
-}
-
-export interface Game {
-  id: string;
-  background_image: string;
-  name: string;
-  platforms: Platform[];
-  metacritic: number;
-  genres: Genre[];
-}
-
-export interface Genre {
-  name: string;
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
+const useGenres = () => {
+  const [genres, setGenres] = useState<string[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
+
+
+  const getGenres = (data: Game[]): string[] => {
+    const allGenres = data.reduce((acc, game) => {
+      game.genres.forEach(genre => {
+        if (!acc.includes(genre)) acc.push(genre);
+      });
+      return acc;
+    }, [] as Genre[]);
+
+    const genres: string[] = allGenres.map(genre => {
+      return genre.name;
+    });
+
+    console.log(genres);
+
+    return [...new Set(genres)];
+  };
 
   useEffect(() => {
     const controller = new AbortController();
@@ -33,7 +33,7 @@ const useGames = () => {
     apiClient
       .get<Game[]>("/games/random/10", { signal: controller.signal })
       .then(res => {
-        setGames(res.data);
+        setGenres(getGenres(res.data));
         setLoading(false);
       })
       .catch((err: Error) => {
@@ -46,7 +46,7 @@ const useGames = () => {
     return () => controller.abort();
   }, []);
 
-  return { games, error, isLoading };
+  return { genres, error, isLoading };
 };
 
-export default useGames;
+export default useGenres;
