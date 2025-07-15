92908f9cdf6d1f9c228184eed415168b6ed1ba5f
mayankgupta804 <mayank.gupta804@gmail.com>
2025-07-15 09:43:01 +0530

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
-import { List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
@@ -9,7 +10,10 @@ const GenreList = () => {
       {data?.results.map((genre) => {
         return (
           <ListItem paddingY='5px' key={genre.id}>
-            <Text fontSize='lg'>{genre.name}</Text>
+            <HStack>
+              <Image boxSize='32px' borderRadius='8' src={getCroppedImageUrl(genre.image_background)} />
+              <Text fontSize='lg'>{genre.name}</Text>
+            </HStack>
           </ListItem>
         );
       })}
